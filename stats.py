14c8aaadaa345d64e9a8ff0af00ea29366486713
Colombo <colombo.carlos@gmail.com>
2025-03-10 13:34:06 -0600

    taking out hard code to read CLI input

--- stats.py
+++ stats.py
@@ -1,31 +1,28 @@
-#Defining Book to Read
-book = "books/frankenstein.txt"
-
 #Opening the file:
-def open_and_read_file(book):
-    with open(book) as f:
+def open_and_read_file(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
     
     return file_contents
 
 #Spliting Text into words
-def spliting_text_words(book):
-    text = open_and_read_file(book)
+def spliting_text_words(book_path):
+    text = open_and_read_file(book_path)
     splitted_txt = str.split(text)
     
     return splitted_txt
 
 #Counting words
-def word_count(text):
-    splitted_txt = spliting_text_words(text)
+def word_count(book_path):
+    splitted_txt = spliting_text_words(book_path)
     wcount = len(splitted_txt)
     
     return wcount
 
 #Dictionary with character counts
-def characters_dictionary(book):
+def characters_dictionary(book_path):
     char_dict = {}
-    lower_case_text = str.lower(open_and_read_file(book))
+    lower_case_text = str.lower(open_and_read_file(book_path))
     splitted_text = list(lower_case_text)
     
     for char in splitted_text:
@@ -37,8 +34,8 @@ def characters_dictionary(book):
     return char_dict
 
 #Sorting
-def sorting_dictionary(book):
-    char_dict = characters_dictionary(book)
+def sorting_dictionary(book_path):
+    char_dict = characters_dictionary(book_path)
     list_of_dicts = []
     
     for char in char_dict:
@@ -52,14 +49,14 @@ def sorting_dictionary(book):
     return list_of_dicts
     
 
-def main():
+def main(book_path):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count(book)} total words")
+    print(f"Found {word_count(book_path)} total words")
     print("--------- Character Count -------")
 
-    sorted_chars = sorting_dictionary(book)
+    sorted_chars = sorting_dictionary(book_path)
     for char_dict in sorted_chars:
         char = char_dict["character"]
         count = char_dict["count"]
@@ -69,5 +66,3 @@ def main():
             print(f"{char}: {count}")
 
     print("============= END ===============")
-
-main()
